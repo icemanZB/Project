20ba55cac9879084cbef87ce6cef1127469e7d57
icemanZB <254784109@qq.com>
2015-07-08 13:29:49 +0800

    add

--- js/common.js
+++ js/common.js
@@ -53,5 +53,16 @@ var addDate = function (date, days) {
     }
     var result = d.getFullYear() + "/" + month + "/" + day;
     return result;
-}
+};
+
+
+var getPos = function (ev) {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+    return {
+        x: ev.clientX + scrollLeft,
+        y: ev.clientY + scrollTop
+    };
+};
+
 
