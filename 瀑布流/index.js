9d520b3165f9a7c02f1d3e88819916340a0485dd
icemanZB <254784109@qq.com>
2015-10-09 13:12:44 +0800

    add

--- 瀑布流/index.js
+++ 瀑布流/index.js
@@ -6,10 +6,8 @@
  ************************************************************
  */
 ;
-(function($) {
-    var
-    //参数
-        setting = {
+(function ($) {
+    var setting = {
             column_width: 204, //列宽
             column_className: 'waterfall_column', //列的类名
             column_space: 10, //列间距
@@ -19,31 +17,31 @@
             fadein: true, //是否渐显载入
             fadein_speed: 600, //渐显速率，单位毫秒
             insert_type: 1, //单元格插入方式，1为插入最短那列，2为按序轮流插入
-            getResource: function(index) {} //获取动态资源函数,必须返回一个砖块元素集合,传入参数为加载的次数
+            getResource: function (index) {
+            } // 获取动态资源函数,必须返回一个砖块元素集合,传入参数为加载的次数
         },
-        //
-        waterfall = $.waterfall = {}, //对外信息对象
-        $container = null; //容器
-        waterfall.load_index = 0, //加载次数
-        $.fn.extend({
-            waterfall: function(opt) {
-                opt = opt || {};
-                setting = $.extend(setting, opt);
-                $container = waterfall.$container = $(this);
-                waterfall.$columns = creatColumn();
-                render($(this).find(setting.cell_selector).detach(), false); //重排已存在元素时强制不渐显
-                waterfall._scrollTimer2 = null;
-                $(window).bind('scroll', function() {
-                    clearTimeout(waterfall._scrollTimer2);
-                    waterfall._scrollTimer2 = setTimeout(onScroll, 300);
-                });
-                waterfall._scrollTimer3 = null;
-                $(window).bind('resize', function() {
-                    clearTimeout(waterfall._scrollTimer3);
-                    waterfall._scrollTimer3 = setTimeout(onResize, 300);
-                });
-            }
-        });
+    waterfall = $.waterfall = {}, // 对外信息对象
+    $container = null; //容器
+    waterfall.load_index = 0; //加载次数
+    $.fn.extend({
+        waterfall: function (opt) {
+            opt = opt || {};
+            setting = $.extend(setting, opt);
+            $container = waterfall.$container = $(this);
+            waterfall.$columns = creatColumn();
+            render($(this).find(setting.cell_selector).detach(), false); //重排已存在元素时强制不渐显
+            waterfall._scrollTimer2 = null;
+            $(window).bind('scroll', function () {
+                clearTimeout(waterfall._scrollTimer2);
+                waterfall._scrollTimer2 = setTimeout(onScroll, 300);
+            });
+            waterfall._scrollTimer3 = null;
+            $(window).bind('resize', function () {
+                clearTimeout(waterfall._scrollTimer3);
+                waterfall._scrollTimer3 = setTimeout(onResize, 300);
+            });
+        }
+    });
 
     function creatColumn() { //创建列
         waterfall.column_num = calculateColumns(); //列数
@@ -58,16 +56,17 @@
 
     function calculateColumns() { //计算需要的列数
         var num = Math.floor(($container.innerWidth()) / (setting.column_width + setting.column_space));
+        // 保证至少有一列
         if (num < 1) {
             num = 1;
-        } //保证至少有一列
+        }
         return num;
     }
 
     function render(elements, fadein) { //渲染元素
         if (!$(elements).length) return; //没有元素
         var $columns = waterfall.$columns;
-        $(elements).each(function(i) {
+        $(elements).each(function (i) {
             if (!setting.auto_imgHeight || setting.insert_type == 2) { //如果给出了图片高度，或者是按顺序插入，则不必等图片加载完就能计算列的高度了
                 if (setting.insert_type == 1) {
                     insert($(elements).eq(i), setting.fadein && fadein); //插入元素
@@ -79,7 +78,7 @@
             if ($(this)[0].nodeName.toLowerCase() == 'img' || $(this).find(setting.img_selector).length > 0) { //本身是图片或含有图片
                 var image = new Image;
                 var src = $(this)[0].nodeName.toLowerCase() == 'img' ? $(this).attr('src') : $(this).find(setting.img_selector).attr('src');
-                image.onload = function() { //图片加载后才能自动计算出尺寸
+                image.onload = function () { //图片加载后才能自动计算出尺寸
                     image.onreadystatechange = null;
                     if (setting.insert_type == 1) {
                         insert($(elements).eq(i), setting.fadein && fadein); //插入元素
@@ -87,8 +86,8 @@
                         insert2($(elements).eq(i), i, setting.fadein && fadein); //插入元素
                     }
                     image = null;
-                }
-                image.onreadystatechange = function() { //处理IE等浏览器的缓存问题：图片缓存后不会再触发onload事件
+                };
+                image.onreadystatechange = function () { //处理IE等浏览器的缓存问题：图片缓存后不会再触发onload事件
                     if (image.readyState == "complete") {
                         image.onload = null;
                         if (setting.insert_type == 1) {
@@ -98,7 +97,7 @@
                         }
                         image = null;
                     }
-                }
+                };
                 image.src = src;
             } else { //不用考虑图片加载
                 if (setting.insert_type == 1) {
@@ -133,7 +132,7 @@
     function calculateLowest() { //计算最短的那列的索引
         var min = waterfall.$columns.eq(0).outerHeight(),
             min_key = 0;
-        waterfall.$columns.each(function(i) {
+        waterfall.$columns.each(function (i) {
             if ($(this).outerHeight() < min) {
                 min = $(this).outerHeight();
                 min_key = i;
@@ -146,10 +145,11 @@
         $.waterfall.load_index++;
         return setting.getResource($.waterfall.load_index, public_render);
     }
+
     waterfall._scrollTimer = null; //延迟滚动加载计时器
     function onScroll() { //滚动加载
         clearTimeout(waterfall._scrollTimer);
-        waterfall._scrollTimer = setTimeout(function() {
+        waterfall._scrollTimer = setTimeout(function () {
             var $lowest_column = waterfall.$columns.eq(calculateLowest()); //最短列
             var bottom = $lowest_column.offset().top + $lowest_column.outerHeight(); //最短列底部距离浏览器窗口顶部的距离
             var scrollTop = document.documentElement.scrollTop || document.body.scrollTop || 0; //滚动条距离
