f26eb6d4117e60d22a862d00bcebf0851e2ac6b7
icemanZB <254784109@qq.com>
2015-10-23 22:46:38 +0800

    add

--- 移动端项目/手机相册/main.js
+++ 移动端项目/手机相册/main.js
@@ -1,9 +1,44 @@
 create(aData);
+function open3D(obj) {
+    var aDiv = obj.getElementsByTagName("div");
+    for (var i = 0; i < aDiv.length; i++) {
+        aDiv[i].style.transition = "0.5s";
+        aDiv[i].className = "show";
+    }
+}
+function clos3D(obj, oLi) {
+    var aDiv = obj.getElementsByTagName("div");
+    aDiv[aDiv.length - 1].addEventListener("webkitTransitionEnd", fn, false);
+    aDiv[aDiv.length - 1].addEventListener("transitionend", fn, false);
+    for (var i = 0; i < aDiv.length; i++) {
+        aDiv[i].style.transition = "0.5s";
+        aDiv[i].className = "";
+    }
+    function fn() {
+        var oPage = document.getElementById("page");
+        aDiv[aDiv.length - 1].removeEventListener("webkitTransitionEnd", fn, false);
+        aDiv[aDiv.length - 1].removeEventListener("transitionend", fn, false);
+        oLi.style.opacity = 1;
+        oPage.removeChild(obj);  // 去掉3d图的div
+    }
+}
+function getOffset(obj) {
+    var l = 0;
+    var t = 0;
+    while (obj) {
+        l += obj.offsetLeft;
+        t += obj.offsetTop;
+        obj = obj.offsetParent;
+    }
+    return {l: l, t: t};
+}
 function create(aData) {
     var oPage = document.getElementById("page");
     var oList = document.getElementsByClassName("picList")[0];
     var aLi = oList.getElementsByTagName("li");
     var aBtns = oPage.getElementsByClassName("btn");
+    var aRecycle = oPage.getElementsByClassName("recycle");
+    var aPicBox = oPage.getElementsByClassName("picBox");
     var sHtml = "";
     var aRemove = [];  // 存删除的图片
     var bOff = true;
@@ -38,13 +73,15 @@ function create(aData) {
             }
         }
         else {
-            aBtns[1].innerHTML = "选择";
-            aBtns[0].style.display = "none";
             for (var j = 0; j < aLi.length; j++) {
-                aLi[j].style.opacity = 1;
                 aLi[j].removeEventListener("touchend", fnSelected, false); // 取消后删除事件
             }
+            for (var k = 0; k < aRemove.length; k++) {
+                clos3D(aPicBox[k], aLi[aRemove[k]]);
+            }
             aRemove.length = 0;  // 清空数组
+            aBtns[1].innerHTML = "选择";
+            aBtns[0].style.display = "none";
         }
         bOff = !bOff;
     }
@@ -52,9 +89,24 @@ function create(aData) {
     // 图片选中事件
     function fnSelected() {
         aRemove.push(this.index);  // 点击的时候把删除图片的index push到arr中
-        console.log(this);
-        this.style.opacity = 0.1;
+        this.style.opacity = 0;
         aBtns[0].style.display = "block";
+        aRecycle[0].style.top = aRecycle[1].style.top = "calc(100% - 0.4rem)";
+        create3d(this);
+    }
+
+    function create3d(oLi) {
+        var oDiv = document.createElement("div");
+        var oXy = getOffset(oLi);
+        oDiv.className = "picBox";
+        oDiv.style.backgroundImage = oLi.style.backgroundImage;
+        oDiv.style.left = oXy.l + "px";
+        oDiv.style.top = oXy.t + "px";
+        oDiv.innerHTML = '<div><div style="background-position:-0.1rem 0;"><div style="background-position:-0.2rem 0;"><div style="background-position:-0.3rem 0;"><div style="background-position:-0.4rem 0;"><div style="background-position:-0.5rem 0;"><div style="background-position:-0.6rem 0;"><div style="background-position:-0.7rem 0;"><div style="background-position:-0.8rem 0;"><div style="background-position:-0.9rem 0;"></div></div></div></div></div></div></div></div></div></div>';
+        oPage.appendChild(oDiv);
+        setTimeout(function () {
+            open3D(oDiv);
+        }, 30);
     }
 
     function fnRemove() {
@@ -62,12 +114,32 @@ function create(aData) {
         aRemove = aRemove.sort(function (a, b) {
             return a - b;
         });
-        while (aRemove.length) {
-            var iNum = aRemove.pop(); // 从后往前删，为了不改变数组中的顺序
-            oList.removeChild(aLi[iNum]);
+        // 默认设置的top是100%，所以是100% - 0.6rem  0.6是测试后的数字
+        aRecycle[0].style.top = aRecycle[1].style.top = "calc(100% - 0.6rem)";
+        for (var i = 0; i < aRemove.length; i++) {
+            fnDle(i);
         }
-        bOff = false;
-        toPosition();
-        fnEnd();
+        setTimeout(function () {
+            aRecycle[0].className = aRecycle[1].className = "recycle recycleShow";
+            while (aRemove.length) {
+                oPage.removeChild(aPicBox[aRemove.length - 1]);
+                var iNub = aRemove.pop();   // 从后往前删，为了不改变数组中的顺序
+                oList.removeChild(aLi[iNub]);
+            }
+            toPosition();
+            fnEnd();
+        }, 650);
+        // 隐藏垃圾桶
+        setTimeout(function () {
+            aRecycle[0].style.top = aRecycle[1].style.top = "100%";
+        }, 1000);
+    }
+
+    function fnDle(i) {
+        var obj = aPicBox[i];
+        // var oLi = aLi[aRemove[i]];
+        obj.style.transition = ".3s left,.5s 0.3s top";
+        obj.style.left = "calc(50% - 0.5rem)"; // 居中
+        obj.style.top = "100%";
     }
 }
