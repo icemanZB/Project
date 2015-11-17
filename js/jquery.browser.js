11808f0985ed26727a40f2e04f7ac2be2eecdfc3
icemanZB <254784109@qq.com>
2015-11-17 22:30:55 +0800

    add

--- js/jquery.browser.js
+++ js/jquery.browser.js
@@ -1,19 +1,37 @@
 ;(function ($) {
-    var userAgent = navigator.userAgent.toLowerCase();
 
-    $.browser = {
-        msie: /msie/.test(userAgent),
-        mozilla: /firefox/.test(userAgent),
-        webkit: /webkit/.test(userAgent),
-        opera: /opera/.test(userAgent),
+    var userAgent = navigator.userAgent;
 
-        version: (function () {
+    $.browser = {};
+
+    function uaMatch(ua) {
+        ua = ua.toLowerCase();
+
+        var rwebkit = /(webkit)[ \/]([\w.]+)/,
+            ropera = /(opera)(?:.*version)?[ \/]([\w.]+)/,
+            rmsie = /(msie) ([\w.]+)/,
+            rmozilla = /(mozilla)(?:.*? rv:([\w.]+))?/;
+
+        var match = rwebkit.exec(ua) ||
+            ropera.exec(ua) ||
+            rmsie.exec(ua) ||
+            ua.indexOf("compatible") < 0 && rmozilla.exec(ua) ||
+            [];
+
+        return {browser: match[1] || "", version: match[2] || "0"};
+    }
 
 
-        })()
+    var browserMatch = uaMatch(userAgent);
 
-    };
+    if (browserMatch.browser) {
+        $.browser[browserMatch.browser] = true;
+        $.browser.version = browserMatch.version;
+    }
 
+    if ($.browser.webkit) {
+        $.browser.safari = true;
+    }
 
 
 })(jQuery);
